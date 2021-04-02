b92374e33973b4a008da5bfabeabc8e3c01465fb
Caggegi <rosariocaggegi99@gmail.com>
2021-04-03 01:48:50 +0200

    almost all

--- contents.js
+++ contents.js
@@ -1,132 +1,74 @@
-const film = [
+const video = [
     {
-      titolo: 'Il Re Leone',
-      immagine: 'https://lumiere-a.akamaihd.net/v1/images/b_thelionking2019_header_poststreet_18276_ada305ce.jpeg?region=0,0,2048,879',
-      creator: 'Disney',
-      descrizione: 'Descrizione...'
-    },
-    {
-        titolo: 'Soul',
-        immagine: 'https://www.nerdpool.it/wp-content/uploads/2021/03/soul-cover-disney-pixar-np.jpeg',
+        titolo: 'Il Re Leone',
+        immagine: 'https://lumiere-a.akamaihd.net/v1/images/b_thelionking2019_header_poststreet_18276_ada305ce.jpeg?region=0,0,2048,879',
         creator: 'Disney',
-        descrizione: 'Descrizione...'
-    },
-    {
-        titolo: 'Collateral Beauty',
-        immagine: 'https://www.latestatamagazine.it/wp-content/uploads/2021/01/Collateral-Beauty.jpg',
-        creator: 'Warner Bros. Pictures',
-        descrizione: 'Descrizione...'
-    }]
-const musica = [
+        descrizione: 'Il re leone (The Lion King) è un film del 2019 diretto da Jon Favreau e prodotto dalla Walt Disney Pictures. Si tratta di un remake shot-for-shot fotorealistico di CGI dell\'omonimo film del 1994.',
+        id: '000',
+        tipo: 'film'
+        
+      },
+      {
+          titolo: 'Soul',
+          immagine: 'https://www.nerdpool.it/wp-content/uploads/2021/03/soul-cover-disney-pixar-np.jpeg',
+          creator: 'Disney',
+          descrizione: 'Soul is a 2020 American computer-animated fantasy comedy-drama film produced by Pixar Animation Studios and released by Walt Disney Pictures. Directed by Pete Docter and co-directed by Kemp Powers, the film stars the voices of Jamie Foxx, Tina Fey, Graham Norton, Rachel House, Alice Braga, Richard Ayoade, Phylicia Rashad, Donnell Rawlings, Questlove, and Angela Bassett. The story follows a middle school music teacher named Joe Gardner, who seeks to reunite his soul and his body after they are accidentally separated, just before his big break as a jazz musician. Soul is the first Pixar film to feature an African-American protagonist.',
+          id: '001',
+          tipo: 'film'
+      },
+      {
+          titolo: 'Collateral Beauty',
+          immagine: 'https://www.latestatamagazine.it/wp-content/uploads/2021/01/Collateral-Beauty.jpg',
+          creator: 'Warner Bros. Pictures',
+          descrizione: 'Collateral Beauty is a 2016 American fantasy drama film directed by David Frankel and written by Allan Loeb. The film stars an ensemble cast of Will Smith, Edward Norton, Keira Knightley, Michael Peña, Naomie Harris, Jacob Latimore, Kate Winslet, and Helen Mirren.',
+          id: '002',
+          tipo: 'film'
+      },
     {
         titolo: 'Voce',
         immagine: 'https://www.oaplus.it/wp-content/uploads/2021/03/Madame-Cover-Voce-Singolo-Sanremo-2021.jpg',
         creator: 'Madame',
-        descrizione: 'Descrizione...'
+        descrizione: 'Mi ricordo di te \nRicordo i mille giri sulle giostre, su di te\nHo fatto un\'altra canzone, mi ricorda chi sono\nHo messo un altro rossetto sopra il labbro superiore...',
+        id: '003',
+        tipo: 'musica'
     },
     {
           titolo: 'Never Gonna Give You Up',
           immagine: 'https://i.ytimg.com/vi/dQw4w9WgXcQ/maxresdefault.jpg',
           creator: 'Rick Astley',
-          descrizione: 'Descrizione...'
+          descrizione: '"Never Gonna Give You Up" is the debut single recorded by English singer and songwriter Rick Astley, released on 27 July 1987. It was written and produced by Stock Aitken Waterman, and was released as the first single from Astley\'s debut album, Whenever You Need Somebody (1987). The song was a worldwide number-one hit, initially in the United Kingdom in 1987, where it stayed at the top of the chart for five weeks and was the best-selling single of that year. It eventually topped the charts in 25 countries, including the United States and West Germany.[6] The song won Best British Single at the 1988 Brit Awards.',
+          id: '004',
+          tipo: 'musica'
     },
     {
           titolo: 'Twisted Transistor',
           immagine: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e8/Twisted_Transistor.jpg/220px-Twisted_Transistor.jpg',
           creator: 'Korn',
-          descrizione: 'Descrizione...'
-    }]
-const gameplay = [
+          descrizione: 'Hey you, hey you, devil\'s little sister\nListening to your twisted transistor\nHold it between your legs\nTurn it up, turn it up\nThe wind is coming through\nCan\'t get enough...',
+          id: '005',
+          tipo:'musica'
+    },
     {
         titolo: 'League of Legends',
         immagine: 'https://www.outplayed.it/wp-content/uploads/2019/09/league-of-legends.jpg',
         creator: 'Riot',
-        descrizione: 'Descrizione...'
+        descrizione: 'League of Legends is a 2009 multiplayer online battle arena video game developed and published by Riot Games. Inspired by Defense of the Ancients, a custom map for Warcraft III, Riot\'s founders sought to develop a stand-alone game in the same genre. Since its release in October 2009, the game has been free-to-play, and is monetized through purchasable character customization. The game is available for Microsoft Windows and macOS.',
+        id: '006',
+        tipo: 'gameplay'
     },
     {
         titolo: 'Minecraft',
         immagine: 'https://d2e111jq13me73.cloudfront.net/sites/default/files/styles/amp_blog_image_large/public/blog/csm-blog/parentsultimateguide-minecraft-blog.jpg?itok=2beDaNo7',
         creator: 'Mojang',
-        descrizione: 'Descrizione...'
+        descrizione: 'Minecraft is a sandbox video game developed by Mojang. The game was created by Markus "Notch" Persson in the Java programming language. Following several early test versions, it was released as a paid public alpha for personal computers in 2009 before releasing in November 2011, with Jens Bergensten taking over development. Minecraft has since been ported to several other platforms and is the best-selling video game of all time, with 200 million copies sold and 126 million monthly active users as of 2020.',
+        id: '007',
+        tipo: 'gameplay'
     },
     {
         titolo: 'Cyberpunk 2077',
         immagine: 'https://www.gamelegends.it/wp-content/uploads/2021/01/cyberpunk-2077-Main-Theme.jpg',
         creator: 'CD Projekt RED',
-        descrizione: 'Descrizione...'
+        descrizione: 'Cyberpunk 2077 is an open-world, action-adventure role-playing video game, developed and published by CD Projekt RED that was released December 10, 2020. The game was announced during the 2012 CD Projekt Red Summer Conference as the official video game adaption of Mike Pondsmith\'s pen and paper RPG Cyberpunk 2020. Originally titled Cyberpunk, the 2077 subtitle was added in late 2012.',
+        id: '008',
+        tipo: 'gameplay'
     }]
-
-    for(let elemento of film){
-        const sezione_film = document.querySelector("section#film div.show-case");
-        const carta = document.createElement("div");
-        carta.classList.add("card");
-        const immagine = document.createElement("img");
-        immagine.src = elemento.immagine;
-        immagine.classList.add("image");
-        const about = document.createElement("div");
-        const titolo = document.createElement("h5");
-        titolo.textContent=elemento.titolo;
-        const creator = document.createElement("p");
-        creator.textContent=elemento.creator;
-        const plus = document.createElement("img");
-        const info = document.createElement("img");
-        plus.src="img/heart-plus.svg";
-        info.src="img/information.svg";
-        about.appendChild(titolo);
-        about.appendChild(creator);
-        about.appendChild(plus);
-        about.appendChild(info);
-        carta.appendChild(immagine);
-        carta.appendChild(about);
-        sezione_film.appendChild(carta);
-    }
-
-    for(let elemento of musica){
-        const sezione_musica = document.querySelector("section#musica div.show-case");
-        const carta = document.createElement("div");
-        carta.classList.add("card");
-        const immagine = document.createElement("img");
-        immagine.src = elemento.immagine;
-        immagine.classList.add("image");
-        const about = document.createElement("div");
-        const titolo = document.createElement("h5");
-        titolo.textContent=elemento.titolo;
-        const creator = document.createElement("p");
-        creator.textContent=elemento.creator;
-        const plus = document.createElement("img");
-        const info = document.createElement("img");
-        plus.src="img/heart-plus.svg";
-        info.src="img/information.svg";
-        about.appendChild(titolo);
-        about.appendChild(creator);
-        about.appendChild(plus);
-        about.appendChild(info);
-        carta.appendChild(immagine);
-        carta.appendChild(about);
-        sezione_musica.appendChild(carta);
-    }
-
-    for(let elemento of gameplay){
-        const sezione_gameplay = document.querySelector("section#gameplay div.show-case");
-        const carta = document.createElement("div");
-        carta.classList.add("card");
-        const immagine = document.createElement("img");
-        immagine.src = elemento.immagine;
-        immagine.classList.add("image");
-        const about = document.createElement("div");
-        const titolo = document.createElement("h5");
-        titolo.textContent=elemento.titolo;
-        const creator = document.createElement("p");
-        creator.textContent=elemento.creator;
-        const plus = document.createElement("img");
-        const info = document.createElement("img");
-        plus.src="img/heart-plus.svg";
-        info.src="img/information.svg";
-        about.appendChild(titolo);
-        about.appendChild(creator);
-        about.appendChild(plus);
-        about.appendChild(info);
-        carta.appendChild(immagine);
-        carta.appendChild(about);
-        sezione_gameplay.appendChild(carta);
-    }
--- script.js
+++ script.js
@@ -1,8 +1,236 @@
-console.log("caricata");
 
-function cercato(){
-    console.log("sto cercando");
+    var film=0;
+    var musica=0;
+    var gameplay=0;
+    for(let elemento of video){
+        if(elemento.tipo=='film'){
+            film++;
+        } else if(elemento.tipo=='musica'){
+            musica++;
+        } else if(elemento.tipo == 'gameplay'){
+            gameplay++;
+        }
+    }
+    if(film===0){
+        document.querySelector("section#film").classList.add("hide");
+        document.querySelector("section#film").classList.remove("show");
+    } else{
+        document.querySelector("section#film").classList.remove("hide");
+        document.querySelector("section#film").classList.add("show");
+    }
+    if(musica===0){
+        document.querySelector("section#musica").classList.add("hide");
+        document.querySelector("section#musica").classList.remove("show");
+    } else{
+        document.querySelector("section#musica").classList.remove("hide");
+        document.querySelector("section#musica").classList.add("show");
+    }
+    if(gameplay===0){
+        document.querySelector("section#gameplay").classList.add("hide");
+        document.querySelector("section#gameplay").classList.remove("show");
+    } else{
+        document.querySelector("section#gameplay").classList.remove("hide");
+        document.querySelector("section#gameplay").classList.add("show");
+    }
+    
+    for(let elemento of video){
+        var sezione;
+        if(elemento.tipo=='film'){
+            sezione = document.querySelector("section#film div.show-case");
+        } else if(elemento.tipo=='musica'){
+            sezione = document.querySelector("section#musica div.show-case");
+        } else if(elemento.tipo == 'gameplay'){
+            sezione = document.querySelector("section#gameplay div.show-case");
+        }
+        create_card(sezione, elemento, true);
+    }
+
+const preferiti = [];
+const ricerca = [];
+
+
+function mostraDescrizione(event){
+    const button = event.currentTarget;
+    const cards = document.querySelectorAll("div.card");
+    console.log("mostro"+button.dataset.tipo+button.dataset.codice);
+    for(card of cards){
+        if(card.dataset.codice === button.dataset.codice && card.dataset.tipo === button.dataset.tipo){
+            const descrizione = card.querySelector("p.hide");
+            if (descrizione !== null){
+                descrizione.classList.add("show");
+                descrizione.classList.remove("hide");
+            }
+            else{
+                const descrizione = card.querySelector("p.description");
+                descrizione.classList.add("hide");
+                descrizione.classList.remove("show");
+            }
+        }
+    }
+
 }
 
-const cerca = document.querySelector("header div#search div img");
-cerca.addEventListener("click", cercato);
+function aggiungiPreferiti(event){
+    const type = event.currentTarget.dataset.tipo;
+    const id = event.currentTarget.dataset.codice;
+    if(preferiti.length===0){
+        document.querySelector("section#preferiti").classList.add("show");
+        document.querySelector("section#preferiti").classList.remove("hide");
+    }
+    for(elemento of video){
+        if(elemento.tipo===type && elemento.id===id){
+            if(preferiti.indexOf(elemento)===-1){
+                preferiti.push(elemento);
+                sezione = document.querySelector("section#preferiti div.show-case");
+                create_card(sezione, elemento, false);
+            }
+        }
+    }
+}
+
+function rimuoviPreferiti(event){
+    const type = event.currentTarget.dataset.tipo;
+    const id = event.currentTarget.dataset.codice;
+    const cards = document.querySelectorAll("section#preferiti div.card");
+    const sezione = document.querySelector("section#preferiti div");
+    for(card of cards){
+        if(card.dataset.tipo==type && card.dataset.codice==id){
+            sezione.removeChild(card);
+        }
+    }
+    for(pref of preferiti){
+        if(pref.tipo===type && pref.id===id){
+            preferiti.splice(preferiti.indexOf(pref),1);
+        }
+    }
+    if(preferiti.length===0){
+        document.querySelector("section#preferiti").classList.add("hide");
+        document.querySelector("section#preferiti").classList.remove("show");
+    }
+}
+
+
+const info_button = document.querySelectorAll("div.card div img.info");
+for (button of info_button){
+    button.addEventListener("click", mostraDescrizione);
+}
+
+
+const favourites = document.querySelectorAll("div.card div img.preferiti");
+for (button of favourites){
+    button.addEventListener("click", aggiungiPreferiti);
+}
+
+
+function avviaRicerca(){
+    ricerca.splice(0, ricerca.length);
+    const barra_di_ricerca = document.querySelector("header div#search input#search");
+    testo = barra_di_ricerca.value;
+    const sezione_ricerca = document.querySelector("section#ricerca div.show-case");
+    while(sezione_ricerca.firstChild){
+        sezione_ricerca.removeChild(sezione_ricerca.firstChild);
+    }
+    if(testo!==""){
+        for(let content of video){
+            if(content.titolo.toLowerCase().includes(testo.toLowerCase()) || content.creator.toLowerCase().includes(testo.toLowerCase())){
+                ricerca.push(content);
+                console.log(sezione_ricerca);
+                create_card(sezione_ricerca, content, true);
+            }
+            }
+        if (ricerca.length!==0){
+            showsearch();
+        } else{
+            hidesearch();
+        }
+    } else{
+        hidesearch();
+    }
+}
+
+const barra_di_ricerca = document.querySelector("header div#search input#search");
+barra_di_ricerca.addEventListener("keyup", avviaRicerca);
+
+function hidesearch(){
+    document.querySelector("section#ricerca").classList.add("hide");
+    document.querySelector("section#ricerca").classList.remove("show");
+    document.querySelector("section#film").classList.remove("hide");
+    document.querySelector("section#film").classList.add("show");
+    document.querySelector("section#musica").classList.remove("hide");
+    document.querySelector("section#musica").classList.add("show");
+    document.querySelector("section#gameplay").classList.remove("hide");
+    document.querySelector("section#gameplay").classList.add("show");
+    if(preferiti.length!==0){
+        document.querySelector("section#preferiti").classList.remove("hide");
+        document.querySelector("section#preferiti").classList.add("show");
+    }
+}
+
+function showsearch(){
+    document.querySelector("section#ricerca").classList.add("show");
+    document.querySelector("section#ricerca").classList.remove("hide");
+    document.querySelector("section#film").classList.remove("show");
+    document.querySelector("section#film").classList.add("hide");
+    document.querySelector("section#musica").classList.remove("show");
+    document.querySelector("section#musica").classList.add("hide");
+    document.querySelector("section#gameplay").classList.remove("show");
+    document.querySelector("section#gameplay").classList.add("hide");
+    document.querySelector("section#preferiti").classList.remove("show");
+    document.querySelector("section#preferiti").classList.add("hide");
+}
+
+function create_card(sezione, elemento, preferiti){
+    if(preferiti===true){
+        _pref='preferiti';
+        _img='img/heart-plus.svg';
+    } else{
+        _pref='rimuovi';
+        _img='img/heart-minus.svg';
+    }
+    const carta = document.createElement("div");
+    carta.classList.add("card");
+    const immagine = document.createElement("img");
+    immagine.src = elemento.immagine;
+    immagine.classList.add("image");
+    const about = document.createElement("div");
+    const titolo = document.createElement("h5");
+    titolo.textContent=elemento.titolo;
+    const creator = document.createElement("p");
+    creator.textContent=elemento.creator;
+    const descrizione = document.createElement("p");
+    descrizione.textContent=elemento.descrizione;
+    descrizione.classList.add("hide");
+    descrizione.classList.add("description");
+    const plus = document.createElement("img");
+    const info = document.createElement("img");
+    plus.src=_img;
+    plus.dataset.codice = elemento.id;
+    plus.dataset.tipo = elemento.tipo;
+    info.src="img/information.svg";
+    info.dataset.codice = elemento.id;
+    info.dataset.tipo = elemento.tipo;
+    plus.classList.add(_pref);
+    info.classList.add("info");
+    about.appendChild(titolo);
+    about.appendChild(creator);
+    about.appendChild(descrizione);
+    about.appendChild(plus);
+    about.appendChild(info);
+    carta.appendChild(immagine);
+    carta.appendChild(about);
+    carta.dataset.codice = elemento.id;
+    carta.dataset.tipo = elemento.tipo;
+    sezione.appendChild(carta);
+    const not_favourites = document.querySelectorAll("div.card div img.rimuovi");
+    for (pulsante of not_favourites){
+        pulsante.addEventListener("click", rimuoviPreferiti);
+    }
+    const favourites = document.querySelectorAll("div.card div img.preferiti");
+    for (pulsante of favourites){
+        pulsante.addEventListener("click", aggiungiPreferiti);
+    }
+    const info_button = document.querySelectorAll("div.card div img.info");
+    for (button of info_button){
+        button.addEventListener("click", mostraDescrizione);
+    }
+}
